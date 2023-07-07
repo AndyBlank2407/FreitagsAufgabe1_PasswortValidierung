c65120e1a7980ce6638ab64ae4dba902293c8c84
andreas <andreas.blank.1@gmx.de>
2023-07-07 16:24:13 +0200

    Erste Tests

--- src/main/java/de/neueFische/Main.java
+++ src/main/java/de/neueFische/Main.java
@@ -1,11 +1,112 @@
 package de.neueFische;
 
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Helo");
-
-        }
+        System.out.println(goodPassWord("0000000011"));
     }
+
+
+    public static String passwLength(String password) {
+
+        if (password.length() < 8) {
+            return "Passwort nicht sicher. Passwort muss mindestens 8 Stellen haben!";
+        }
+
+        return "Passwort ist genug lang (über 8 Stellen.";
+    }
+
+    public static boolean passwContainsNumbers(String password) {
+
+        if (password.matches("[a-zA-Z]+")) {
+            return false;
+        }
+        return true;
+    }
+
+/*    public static boolean passwOnlyUpperCase(String password){
+        if(password.matches("[0-9]+")){
+            System.out.println("Nur Zahlen, daher kann nicht auf Upper-Case geprüft werden!");
+            return false;
+        }
+
+        if(password.matches("[A-Z0-9]+")){
+            return true;
+        }
+        return false;
+    }
+*/
+
+
+ /*   public static String checkIfPasswContUpper_LowerCase(String password){
+        boolean upperCase = false;
+        boolean lowerCase = false;
+
+        char[] charsOfPassw = password.toCharArray();
+        for(char element : charsOfPassw){
+            if(Character.isDigit(element)){
+                continue;
+            }
+            if(Character.isUpperCase(element)){
+                upperCase = true;
+            }
+            else if (Character.isLowerCase(element)) {
+                lowerCase = true;
+            }
+        }
+        if(upperCase && lowerCase){
+            return "Groß- und Kleinbuchstaben enthalten!";
+        }
+        else if(upperCase){
+            return "Nur Großbuchstaben enthalten!";
+        }
+        else if(lowerCase){
+            return "Nur Kleinbuchstaben enthalten!";
+        }
+        return "Keine Buchstaben enthalten";
+    }
+
+  */
+
+    public static boolean checkIfPasswContUpperAndLowerCase(String password) {
+        boolean upperCase = false;
+        boolean lowerCase = false;
+        char[] charsOfPassw = password.toCharArray();
+
+        for (char element : charsOfPassw) {
+            if (Character.isDigit(element)) {
+                continue;
+            }
+            if (Character.isUpperCase(element)) {
+                upperCase = true;
+            }
+            else if(Character.isLowerCase(element)){
+                lowerCase = true;
+            }
+        }
+        if (upperCase && lowerCase) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static boolean goodPassWord(String password) {
+        if (password.equals("Passwort") || password.equals(("passwort"))
+                || password.equals("12345678") || password.equals("23456789") ||
+                password.equals("00000000") || password.equals("")) {
+            return false;
+        }
+        return true;
+    }
+
+    public static String passwordValid(String password) {
+        String sample = password;
+
+
+        return "Passwort gespeichert";
+
+    }
+}
--- src/test/java/de/neueFische/MainTest.java
+++ src/test/java/de/neueFische/MainTest.java
@@ -1,7 +1,172 @@
 package de.neueFische;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    @Test
+    void passwLengthChecked_With7Chars(){
+        //given
+        String password = "kkkkkkk";
+        String solution = "Passwort nicht sicher. Passwort muss mindestens 8 Stellen haben!";
+
+        //when
+        String actual = Main.passwLength(password);
+
+        //then
+        assertEquals(solution, actual);
+
+    }
+
+    @Test
+    void passwLengthChecked_With8Chars(){
+        //given
+        String password = "kkkkkkkk";
+        String solution ="Passwort ist genug lang (über 8 Stellen.";
+
+        //when
+        String actual = Main.passwLength(password);
+
+        //then
+        assertEquals(solution, actual);
+
+    }
+
+    @Test
+    void passwContNumbersChecked_WithNumbersAndLowerCase(){
+        //given
+        String password = "k6k";
+        boolean solution = true;
+
+        //when
+        boolean actual = Main.passwContainsNumbers(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void passwContNumbersChecked_WithNumbersAndUpperCase(){
+        //given
+        String password = "K6K";
+        boolean solution = true;
+
+        //when
+        boolean actual = Main.passwContainsNumbers(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void passwContNumbersChecked_WithNoNumbersAndLowerCase(){
+        //given
+        String password = "kkk";
+        boolean solution = false;
+
+        //when
+        boolean actual = Main.passwContainsNumbers(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void passwContNumbersChecked_WithNoNumbersAndUpperCase(){
+        //given
+        String password = "KKK";
+        boolean solution = false;
+
+        //when
+        boolean actual = Main.passwContainsNumbers(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void checkIfPwContUpperAndLowerCaseChecked_WithUpperCaseAndNoNumbers(){
+        //given
+        String password = "JUNITTESTING";
+        boolean solution = false;
+
+        //when
+        boolean actual = Main.checkIfPasswContUpperAndLowerCase(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void checkIfPwContUpperAndLowerCaseChecked_WithUpperCaseAndNumbers(){
+        //given
+        String password = "JUNITTESTI123NG";
+        boolean solution = false;
+
+        //when
+        boolean actual = Main.checkIfPasswContUpperAndLowerCase(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void checkIfPwContUpperAndLowerCaseChecked_WithLowerCaseAndNoNumbers(){
+        //given
+        String password = "junittesting";
+        boolean solution = false;
+
+        //when
+        boolean actual = Main.checkIfPasswContUpperAndLowerCase(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void checkIfPwContUpperAndLowerCaseChecked_WithLowerCaseAndNumbers(){
+        //given
+        String password = "junittest123ing";
+        boolean solution = false;
+
+        //when
+        boolean actual = Main.checkIfPasswContUpperAndLowerCase(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void checkIfPwContUpperAndLowerCaseChecked_WithUpperAndLowerCaseAndNoNumbers(){
+        //given
+        String password = "JUNITTestING";
+        boolean solution = true;
+
+        //when
+        boolean actual = Main.checkIfPasswContUpperAndLowerCase(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+    @Test
+    void checkIfPwContUpperAndLowerCaseChecked_WithUpperAndLowerCaseNoNumbers(){
+        //given
+        String password = "JUNITTest123ING";
+        boolean solution = true;
+
+        //when
+        boolean actual = Main.checkIfPasswContUpperAndLowerCase(password);
+
+        //then
+        assertEquals(solution, actual);
+    }
+
+
+
+
+
 
 }
